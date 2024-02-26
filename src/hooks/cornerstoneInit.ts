5ba2503034ce42cd7aee901f1fca8f10b1ef63d5
ChienChihYeh <miroyeh1986@gmail.com>
2024-02-26 16:29:16 +0800

    remove unused loader from useCornerstoneInit hook

--- src/hooks/cornerstoneInit.ts
+++ src/hooks/cornerstoneInit.ts
@@ -1,12 +1,7 @@
 import * as cornerstone from '@cornerstonejs/core'
-import { volumeLoader } from '@cornerstonejs/core'
 import * as cornerstoneTools from '@cornerstonejs/tools'
 import dicomParser from 'dicom-parser'
 import cornerstoneDICOMImageLoader from '@cornerstonejs/dicom-image-loader'
-import {
-  cornerstoneStreamingImageVolumeLoader,
-  cornerstoneStreamingDynamicImageVolumeLoader
-} from '@cornerstonejs/streaming-image-volume-loader'
 import { onMounted } from 'vue'
 
 export function useCornerstoneInit() {
@@ -15,15 +10,7 @@ export function useCornerstoneInit() {
     cornerstoneTools.init()
     cornerstoneDICOMImageLoader.external.cornerstone = cornerstone
     cornerstoneDICOMImageLoader.external.dicomParser = dicomParser
-    volumeLoader.registerUnknownVolumeLoader(cornerstoneStreamingImageVolumeLoader)
-    volumeLoader.registerVolumeLoader(
-      'cornerstoneStreamingImageVolume',
-      cornerstoneStreamingImageVolumeLoader
-    )
-    volumeLoader.registerVolumeLoader(
-      'cornerstoneStreamingDynamicImageVolume',
-      cornerstoneStreamingDynamicImageVolumeLoader
-    )
+
     const { StackScrollMouseWheelTool, WindowLevelTool, ZoomTool } = cornerstoneTools
     cornerstoneTools.addTool(WindowLevelTool)
     cornerstoneTools.addTool(ZoomTool)
